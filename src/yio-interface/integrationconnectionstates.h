5b02ac5ca55900a49781322a4977551a9459da8e
Michael Loercher <michaelloercher@web.de>
2021-05-18 17:05:10 +0200

    Code Style

--- src/yio-interface/integrationconnectionstates.h
+++ src/yio-interface/integrationconnectionstates.h
@@ -1,6 +1,5 @@
 /******************************************************************************
  *
- * Copyright (C) 2018-2019 Marton Borzak <hello@martonborzak.com>
  * Copyright (C) 2021 Michael Lörcher <MichaelLoercher@web.de>
  *
  * This file is part of the YIO-Remote software project.
